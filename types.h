b60ceeefe934626bae7ccc5c2243e382e8de636e
Danil6969 <20danil02@mail.ru>
2021-01-20 15:09:33 +0300

    Create types.h

---
+++ types.h
@@ -0,0 +1,65 @@
+typedef unsigned char   undefined; //size = 1
+
+typedef unsigned char    byte; //size = 1
+typedef unsigned char    dwfenc; //size = 1
+typedef unsigned short    word; //size = 2
+typedef unsigned int    dword; //size = 4
+typedef long long    longlong; //size = 8
+typedef unsigned long long    qword; //size = 8
+typedef unsigned short    ushort; //size = 2
+typedef unsigned int    uint; //size = 4
+typedef unsigned long    ulong; //size = 4
+typedef unsigned long long    ulonglong; //size = 8
+
+typedef unsigned char    undefined1; //size = 1
+typedef unsigned short    undefined2; //size = 2
+typedef unsigned int    undefined4; //size = 4
+typedef unsigned long long    undefined8; //size = 8
+
+typedef long double float10;
+
+#define unkbyte9   unsigned __int128 //size = 16
+#define unkbyte10   unsigned __int128 //size = 16
+#define unkbyte11   unsigned __int128 //size = 16
+#define unkbyte12   unsigned __int128 //size = 16
+#define unkbyte13   unsigned __int128 //size = 16
+#define unkbyte14   unsigned __int128 //size = 16
+#define unkbyte15   unsigned __int128 //size = 16
+#define unkbyte16   unsigned __int128 //size = 16
+
+#define unkuint9   unsigned __int128 //size = 16
+#define unkuint10   unsigned __int128 //size = 16
+#define unkuint11   unsigned __int128 //size = 16
+#define unkuint12   unsigned __int128 //size = 16
+#define unkuint13   unsigned __int128 //size = 16
+#define unkuint14   unsigned __int128 //size = 16
+#define unkuint15   unsigned __int128 //size = 16
+#define unkuint16   unsigned __int128 //size = 16
+
+#define unkint9   __int128 //size = 16
+#define unkint10   __int128 //size = 16
+#define unkint11   __int128 //size = 16
+#define unkint12   __int128 //size = 16
+#define unkint13   __int128 //size = 16
+#define unkint14   __int128 //size = 16
+#define unkint15   __int128 //size = 16
+#define unkint16   __int128 //size = 16
+
+#define unkfloat1   float //size = 4
+#define unkfloat2   float //size = 4
+#define unkfloat3   float //size = 4
+#define unkfloat4   float //size = 4
+#define unkfloat5   double //size = 8
+#define unkfloat6   double //size = 8
+#define unkfloat7   double //size = 8
+#define unkfloat8   double //size = 8
+#define unkfloat9   __float128 //size = 16
+#define unkfloat10   __float128 //size = 16
+#define unkfloat11   __float128 //size = 16
+#define unkfloat12   __float128 //size = 16
+#define unkfloat13   __float128 //size = 16
+#define unkfloat14   __float128 //size = 16
+#define unkfloat15   __float128 //size = 16
+#define unkfloat16   __float128 //size = 16
+
+//TODO: possibly better float10 support
