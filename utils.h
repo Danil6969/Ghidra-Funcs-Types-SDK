e41726e0ad038568e01cc71d643e0ae7fcd9cf4d
Danil6969 <20danil02@mail.ru>
2021-02-10 21:56:30 +0300

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,5 +1,14 @@
 #include "core.h"
 
+// DO NOT use "_tmp" vars anywhere else in your code!
+
+// also add "static" for anything you return by address in your functions, like:
+// static byte abVar1 [16];
+// ...
+// return abVar1;
+// i.e. you must get rid of "warning: address of stack memory associated with local variable 'abVar1' returned [-Wreturn-stack-address]"
+// or you will get UB especially at -O1 and higher
+
 #define COPY16(x, a) COPY(x, 16, a)
 
 #define TOARR1(x, t) TOARR(x, t, 1)
@@ -15,6 +24,8 @@
 #define CONCAT1_15A(x, y, a) CONCAT(x, y, 1, 15, 16, a)
 #define CONCAT2_2T(x, y, t) CONCATT(x, y, 2, 2, 4, t)
 #define CONCAT2_8T(x, y, t) CONCATT(x, y, 2, 8, 10, t)
+#define CONCAT8_8(x, y) CONCATN(x, y, 8, 8, 16)
+#define CONCAT8_8A(x, y, a) CONCAT(x, y, 8, 8, 16, a)
 
 #define SUB10_2T(x, n, t) SUBT(x, n, 10, 2, t)
 #define SUB16_1T(x, n, t) SUBT(x, n, 16, 1, t)
