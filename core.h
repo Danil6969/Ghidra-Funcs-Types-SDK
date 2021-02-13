0341cfb02c6812704653487ae0108f602618a1ce
Danil6969 <20danil02@mail.ru>
2021-02-13 21:24:03 +0300

    Update core.h

--- core.h
+++ core.h
@@ -7,11 +7,15 @@ void * memset(void *, const int, unsigned long long);
   typeof(x) _tmp1 = (x); \
   *(t*)&_tmp1; })
 
+#define CASTARR(x, t) ({ \
+  typeof(x) _tmp1 = (x); \
+  *(t*)_tmp1; })
+
 #define COPY(x, s, a) memcpy(a, x, s)
 
 #define TOARR(x, t, s) ({ \
   static byte _tmp1[s]; \
-  typeof(x) _tmp2 = (x); \
+  t _tmp2 = (t)(x); \
   memcpy(_tmp1, &_tmp2, s); \
   _tmp1; })
 
@@ -23,12 +27,12 @@ void * memset(void *, const int, unsigned long long);
   byte* _tmp2 = x; \
   byte* _tmp3 = a; \
   memcpy(_tmp3, _tmp1, s2); \
-  memcpy(_tmp3 + s2, _tmp2, s1); \
-  _tmp3; })
+  memcpy(_tmp3 + s2, _tmp2, s1); })
 
 #define CONCATN(x, y, s1, s2, s3) ({ \
   static byte _tmp0[s3]; \
-  CONCAT(x, y, s1, s2, s3, _tmp0); })
+  CONCAT(x, y, s1, s2, s3, _tmp0); \
+  _tmp0; })
 
 #define CONCATT(x, y, s1, s2, s3, t) ({ \
   *(t*)CONCATN(x, y, s1, s2, s3); })
@@ -36,12 +40,12 @@ void * memset(void *, const int, unsigned long long);
 #define SUB(x, n, s1, s2, a) ({ \
   byte* _tmp1 = x; \
   byte* _tmp2 = a; \
-  memcpy(_tmp2, _tmp1 + n, s2); \
-  _tmp2; })
+  memcpy(_tmp2, _tmp1 + n, s2); })
 
 #define SUBN(x, n, s1, s2) ({ \
   static byte _tmp0[s2]; \
-  SUB(x, n, s1, s2, _tmp0); })
+  SUB(x, n, s1, s2, _tmp0); \
+  _tmp0; })
 
 #define SUBT(x, n, s1, s2, t) ({ \
   *(t*)SUBN(x, n, s1, s2); })
@@ -50,5 +54,26 @@ void * memset(void *, const int, unsigned long long);
   byte* _tmp1 = x; \
   byte* _tmp2 = a; \
   memset(_tmp2, 0, s2); \
-  memcpy(_tmp2, _tmp1, s1); \
-  _tmp2; })
+  memcpy(_tmp2, _tmp1, s1); })
+
+#define ZEXTN(x, s1, s2) ({ \
+  static byte _tmp0[s2]; \
+  ZEXT(x, s1, s2, _tmp0); \
+  _tmp0; })
+
+#define ZEXTT(x, s1, s2, t) ({ \
+  *(t*)ZEXTN(x, s1, s2); })
+
+#define SEXT(x, s1, s2, a) ({ \
+  byte* _tmp1 = x; \
+  byte* _tmp2 = a; \
+  memset(_tmp2, 0xff * ((_tmp1[s1 - 1]) >> 7), s2); \
+  memcpy(_tmp2, _tmp1, s1); })
+
+#define SEXTN(x, s1, s2) ({ \
+  static byte _tmp0[s2]; \
+  SEXT(x, s1, s2, _tmp0); \
+  _tmp0; })
+
+#define SEXTT(x, s1, s2, t) ({ \
+  *(t*)SEXTN(x, s1, s2); })
